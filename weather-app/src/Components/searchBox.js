5884258880de18d564db2b2c1251cc060e0a6a5f
Bobby De Luna <bobby.deluna2@gmail.com>
2023-08-30 13:53:41 -0700

    Completed most of the frontend. Might have to go back and reformat containers for when the page is stretched and what not. Also have to reformat the background image, have to crop the pictures and then have shaped to the box.

--- weather-app/src/Components/searchBox.js
+++ weather-app/src/Components/searchBox.js
@@ -3,7 +3,12 @@
 function SearchBox(){
     return(
         <div id="Blam">
-            Blam!
+            <h1 id="searchTitle">Another Weather App</h1>
+            <div id="searchBars">
+                <input id="searchCity" type="type" placeholder="City"></input>
+                <input id="searchState" type="type" placeholder="State"></input>
+            </div>
+            <button id="searchButton" onClick={()=>console.log("Blam!")}>Search</button>
         </div>
     );
 //end
