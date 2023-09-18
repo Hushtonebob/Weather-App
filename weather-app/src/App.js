662002d75eb11423463c2a6d215dfc17b566ce32
Bobby De Luna <bobby.deluna2@gmail.com>
2023-09-17 21:41:57 -0700

    just pushing

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -7,6 +7,10 @@ import './style/darkMode.css';
 import DisplayTab from './Components/displayTab';
 import MoreInfo from './Components/moreInfo';
 import SearchBox from './Components/searchBox';
+import { cities } from './cityArray';
+
+
+
 
 function App() {
 
@@ -14,22 +18,27 @@ function App() {
   const [search, setSearch] = useState("new-york");
   const [display, setDisplay] = useState("New York");
   const [pic, setPic] = useState("new-york");
+  const [change, setChange] = useState(true);
 
-  const [change, setChange] = useState(true)
+  const searchBlam=()=>{
+    setInterval(()=>{setPic(cities[Math.round(Math.random() * 7)]); 
+    setDisplay("Please try again")},10000);
+  }
 
   useEffect(()=>{
     fetch(`https://api.teleport.org/api/urban_areas/slug:${search}/images/`)
     .then(res=>{if(res.ok){
       return res.json();
     }
-    throw new Error("Please search another city"); 
-
+    throw new Error("Please choose another city, or check spelling."); 
   })
     .then(data=>{
-        console.log(data.photos[0].image.web);
         setPic(data.photos[0].image.web);
     })
-    .catch((error)=>{setDisplay("New York");alert(error); setSearch('new-york'); setChange(!change)})
+    .catch((error)=>{ 
+      alert(error); 
+      setChange(!change);
+    })
   }, [change]);
 
   return (
---
+++ weather-app/src/cityArray.js
@@ -0,0 +1,17 @@
+import athens from './cities/athens.jpg';
+import berlin from './cities/berlin.jpg';
+import dubai from './cities/dubai.jpg';
+import hongKong from "./cities/hong-kong.jpg";
+import kyoto from './cities/kyoto.jpg';
+import portland from './cities/portland.jpg';
+import sanFran from './cities/san fran.jpg';
+
+export let cities = [
+   athens, 
+   berlin, 
+   dubai, 
+   hongKong, 
+   kyoto, 
+   portland, 
+   sanFran
+];
